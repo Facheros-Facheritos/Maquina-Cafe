c1d28689fa0626775193407728571d8a19278415
AldairManco <90582878+AldairManco@users.noreply.github.com>
2021-11-23 19:50:03 -0500

    Add files via upload

---
+++ com/mycompany/maquinacafe/NewMain.java
@@ -0,0 +1,8 @@
+package com.mycompany.maquinacafe;
+public class NewMain {
+    public static void main(String[] args) 
+    {
+        CapaPresentacion.frmPrincipal frmPrincipal = new CapaPresentacion.frmPrincipal();
+        frmPrincipal.setVisible(true);
+    }
+}
